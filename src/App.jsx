360ae473a9b91b3024a8ad56fd41cc83b5733fee
harshal239 <harshalwalunj49@gmail.com>
2022-09-12 14:59:10 +0530

    Complete App showcase website

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
+import styles from "./styles/Global";
+function App() {
+  return (
+    <>
+      <SectionWrapper
+        title="You own store of Nifty NFTS"
+        showBtn
+        description="Buy Store,Collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all users devices. You can easily get your app int people's hands."
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to show the store."
+        description="The app contains two screens.The first screen lists all NFTS while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with love by <span>Harshal Walunj</span>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
